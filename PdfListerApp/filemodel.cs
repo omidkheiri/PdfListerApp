38a026861b64883664f71d09063f8c16eeaed453
Omid <omidkheiri@gmail.com>
2025-01-17 00:24:16 +0300

    Add project files.

---
+++ PdfListerApp/filemodel.cs
@@ -0,0 +1,8 @@
+﻿namespace PdfListerApp.PdfListerApp
+{
+    internal class filemodel
+    {
+        [Newtonsoft.Json.JsonProperty("pdfContent", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public byte[] data { get; set; }
+    }
+}
