35230befcab6331bf684e5c52c0dea48ce6a5550
Omid <omidkheiri@gmail.com>
2025-01-17 00:31:08 +0300

    Remove PdfListerApp project files and add initial configuration files

--- PdfListerApp/filemodel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PdfListerApp.PdfListerApp
-{
-    internal class filemodel
-    {
-        [Newtonsoft.Json.JsonProperty("pdfContent", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-        public byte[] data { get; set; }
-    }
-}
